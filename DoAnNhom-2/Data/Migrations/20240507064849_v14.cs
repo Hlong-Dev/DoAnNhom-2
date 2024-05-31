a82c3020656c4ebcc325e5d6d07d179f26f5b562
hlongday <hlongday@DESKTOP-UFGUQD0>
2024-05-31 21:29:02 +0700

    sua choi choi

---
+++ DoAnNhom-2/Data/Migrations/20240507064849_v14.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAnNhom_2.Data.Migrations
+{
+    public partial class v14 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
